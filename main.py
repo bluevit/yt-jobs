1fa0ea2293bc9452c33dbb30a7885478ff6144e5
Muhammad Raza <mrazaiqbal2002@gmail.com>
2025-08-10 02:19:14 +0500

    bugs fixed

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ from dotenv import load_dotenv
 
 # Load environment variables
 load_dotenv()
-WEBHOOK_URL = "https://automationsinc.app.n8n.cloud/webhook/ytjobs"
+WEBHOOK_URL = "https://zealancy.app.n8n.cloud/webhook-test/ytjobs"
 
 CHROMEDRIVER_PATH = "/usr/bin/chromedriver"
 LIST_URL = "https://ytjobs.co/job/search/all_categories"
